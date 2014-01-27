6e7d1294d5f74db5817db288e0bc7df827063ff2
dweintrop <dweintrop@u.northwestern.edu>
2014-01-27 16:57:54 -0600

    Updating plane to refer to correct I18n dictionary file

--- apps/plane/plane.js
+++ apps/plane/plane.js
@@ -31,7 +31,7 @@ var Plane = {};
 // Supported languages.
 BlocklyApps.LANGUAGES = {
   // Format: ['Language name', 'direction', 'XX_compressed.js']
-  en: ['English', 'ltr', 'msg/js/en.js'],
+  en: ['English', 'ltr', 'msg/js/en_us.js'],
   hu: ['Magyar', 'ltr', 'msg/js/en.js'],
   vi: ['Tiếng Việt', 'ltr', 'msg/js/vi.js']
 };
